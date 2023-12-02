bac56a279aa2e6db78715426df4188ef555b1659
shedivesin <>
2023-12-02 14:29:41 -0600

    Revised vocabulary test

--- vocabulary.js
+++ vocabulary.js
@@ -1,7 +1,107 @@
 const readline = require("node:readline/promises");
 
-
 const QUESTIONS = 20;
+const VOCABULARY = [
+  // William Linney, Getting Started with Ancient Greek.
+  ["αἰγιαλός", "beach"],
+  ["αἴλουρος", "cat"],
+  ["ἀγοράζω", "I buy"],
+  ["ἀγρός", "field"],
+  ["ἄγω", "I lead"],
+  ["ἀδελφός", "brother"],
+  ["ἀναγιγνώσκω", "I read"],
+  ["ἀπό", "from"],
+  ["ἄργυρος", "silver"],
+  ["ἄρτος", "bread"],
+  ["βαδίζω", "I walk"],
+  ["βοῦς", "cow"],
+  ["βούτῡρος", "butter"],
+  ["γεωργός", "farmer"],
+  ["γράφω", "I write"],
+  ["δρῡμός", "woods"],
+  ["διδάσκαλος", "teacher"],
+  ["διδάσκω", "I teach"],
+  ["διώκω", "I chase"],
+  ["δίδωμι", "I give"],
+  ["ἐγώ", "I"],
+  ["εἶ", "you are"],
+  ["εἶς", "into"],
+  ["εἰσί(ν)", "they are"],
+  ["εἰμί", "I am"],
+  ["ἐκ/ἐξ", "out of"],
+  ["ἔμπορος", "merchant"],
+  ["ἐν", "in"],
+  ["ἐσθίω", "I eat"],
+  ["ἐστέ", "y'all are"],
+  ["ἐστί(ν)", "he/she/it is", "he is", "she is", "it is"],
+  ["ἐσμέν", "we are"],
+  ["ἔχω", "I have"],
+  ["ἡμεῖς", "we"],
+  ["θεῖος", "uncle"],
+  ["ῑ̓ᾱτρός", "doctor"],
+  ["ἵππος", "horse"],
+  ["καί", "and"],
+  ["καθ’ ἡμέρᾱν", "every day", "daily"],
+  ["καθεύδω", "I sleep"],
+  ["κάλαμος", "pen", "reed", "reed-pen"],
+  ["μάγειρος", "cook", "chef"],
+  ["νῦν", "now"],
+  ["οἶκος", "house"],
+  ["ὅτι", "because"],
+  ["οὐ", "not"],
+  ["οὐκ/οὐχ", "not"],
+  ["οὔποτε", "never"],
+  ["πολλάκις", "often"],
+  ["πάντοτε", "always"],
+  ["ποταμός", "river"],
+  ["πρός", "to/towards", "to", "towards"],
+  ["σπανίως", "seldom"],
+  ["στρατός", "army"],
+  ["στρατηγός", "general", "army-leader"],
+  ["σύ", "you"],
+  ["τρέχω", "I run"],
+  ["τῡρός", "cheese"],
+  ["υἱός", "son"],
+  ["ὑμεῖς", "y'all"],
+  ["φιλόσοφος", "philosopher"],
+  ["χοῖρος", "pig"],
+  ["χόρτος", "fodder"],
+  ["χρῡσός", "gold"],
+
+  // C. W. E. Peckett and A. R. Munday, Thrasymachus.
+  // I
+  ["ἀκούω", "I hear"],
+  ["ἀλλά", "but"],
+  ["ἄνθρωπος", "man", "human"],
+  ["ἄρα", "?"],
+  ["ἀστρσπή", "lightning"],
+  ["βλέπω", "I see", "I look at", "I watch"],
+  ["βροντὴ", "thunder"],
+  ["γάρ", "for"],
+  ["δέ", "but"],
+  ["δεῦρο", "hither", "here"],
+  ["διά", "because of"],
+  ["διὰ τί", "why?"],
+  ["διότι", "because"],
+  ["ἐνδύω", "I put on"],
+  ["ἥκω", "I have come"],
+  ["ἱμάτιον", "cloak", "garment"],
+  ["καταβαίνω", "I go down"],
+  ["κελεύω", "I order"],
+  ["κομίζω", "I take", "I escort"],
+  ["λέγω", "I say", "I speak", "I tell"],
+  ["μή", "don't"],
+  ["νεκρός", "corpse"],
+  ["οἰκία", "house"],
+  ["οἴμοι", "oh dear!"],
+  ["οὖν", "therefore"],
+  ["παιδίον", "child"],
+  ["παρέχω", "I give", "I provide"],
+  ["περί", "about", "concerning"],
+  ["τίς/τί", "who?"],
+  ["φωνή", "voice"],
+  ["χαίρω", "I rejoice"],
+];
 
 
 function shuffle(array) {
@@ -16,125 +116,39 @@ function shuffle(array) {
   return array;
 }
 
-const VOCABULARY = shuffle([
-  // William Linney, Getting Started with Ancient Greek.
-  // [["αἰγιαλός", "beach"]],
-  [["αἴλουρος", "cat"]],
-  [["ἀγοράζω", "I buy"]],
-  // [["ἀγρός", "field"]],
-  [["ἄγω", "I lead"]],
-  [["ἀδελφός", "brother"]],
-  [["ἀναγιγνώσκω", "I read"]],
-  // [["ἀπό", "from"]],
-  [["ἄργυρος", "silver"]],
-  [["ἄρτος", "bread"]],
-  // [["βαδίζω", "I walk"]],
-  [["βοῦς", "cow"]],
-  [["βούτῡρος", "butter"]],
-  [["γεωργός", "farmer"]],
-  [["γράφω", "I write"]],
-  // [["δρῡμός", "woods"]],
-  [["διδάσκαλος", "teacher"]],
-  [["διδάσκω", "I teach"]],
-  [["διώκω", "I chase"]],
-  // [["δίδωμι", "I give"]],
-  [["ἐγώ", "I"]],
-  // [["εἶς", "into"]],
-  [
-    ["εἰμί", "I am"],
-    ["εἶ", "you are"],
-    ["ἐστί(ν)", "he/she/it is", "he is", "she is", "it is"],
-    ["ἐσμέν", "we are"],
-    ["ἐστέ", "y'all are"],
-    ["εἰσί(ν)", "they are"],
-  ],
-  [["ἐκ", "out of"], ["ἐξ", "out of"]],
-  [["ἔμπορος", "merchant"]],
-  [["ἐν", "in"]],
-  [["ἐσθίω", "I eat"]],
-  [["ἔχω", "I have"]],
-  [["ἡμεῖς", "we"]],
-  // [["θεῖος", "uncle"]],
-  [["ῑ̓ᾱτρός", "doctor"]],
-  [["ἵππος", "horse"]],
-  [["καί", "and"]],
-  [["καθ’ ἡμέρᾱν", "every day", "daily"]],
-  [["καθεύδω", "I sleep"]],
-  [["κάλαμος", "pen", "reed", "reed-pen"]],
-  [["μάγειρος", "cook", "chef"]],
-  [["νῦν", "now"]],
-  [["ὁ", "the"], ["οἱ", "the"]],
-  [["οἶκος", "house"]],
-  [["ὅτι", "because"]],
-  [["οὐ", "not"], ["οὐκ", "not"], ["οὐχ", "not"]],
-  [["οὔποτε", "never"]],
-  [["πολλάκις", "often"]],
-  [["πάντοτε", "always"]],
-  [["ποταμός", "river"]],
-  [["πρός", "to/towards", "to", "towards"]],
-  [["σπανίως", "seldom"]],
-  [["στρατός", "army"]],
-  [["στρατηγός", "general", "army-leader"]],
-  [["σύ", "you"]],
-  [["τρέχω", "I run"]],
-  [["τῡρός", "cheese"]],
-  [["υἱός", "son"]],
-  [["ὑμεῖς", "y'all"]],
-  [["φιλόσοφος", "philosopher"]],
-  [["χοῖρος", "pig"]],
-  // [["χόρτος", "fodder"]],
-  [["χρῡσός", "gold"]],
-
-  // C. W. E. Peckett and A. R. Murray, Thrasymachus.
-  [["ἀκούω", "I hear"]],
-  [["ἀστραπή", "lightning"]],
-  [["βλέπω", "I see", "I look at"]],
-  [["βροντὴ", "thunder"]],
-  [["λέγω", "I speak", "I say", "I tell"]],
-  [["παιδίον", "child"]],
-  [["χαίρω", "I rejoice"]],
-
-  // Personal list.
-  [["ἄνθρωπος", "man", "human", "human being"]],
-  [["ἐρίζω", "I quarrel"]],
-  [["ἱμάτιον", "cloak", "garment"]],
-  [["θάλαττα", "sea"]],
-  [["θεός", "god"]],
-  [["κρείσσων", "stronger", "mightier"]],
-  [["ὁπότερος", "which"]],
-  [["μάτην", "in vain"]],
-  [["ποτέ", "once", "at some time"]],
-]);
-
-
-function random(array) {
-  return array[Math.floor(array.length * Math.random())];
-}
-
 async function main() {
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
   });
-  const n = Math.min(QUESTIONS, VOCABULARY.length);
-  const width = Math.floor(Math.log10(n)) + 1;
-  let correct = 0;
+  let n = 0;
 
-  for(let i = 0; i < n; i++) {
-    const [question, ...answers] = random(VOCABULARY[i]);
-    console.log("%s. %s", (i + 1).toString().padStart(width), question);
+  for(let list = shuffle(VOCABULARY.slice()).slice(0, QUESTIONS); list.length !== 0; ) {
+    const next = [];
 
-    const test = await rl.question("> ".padStart(width + 2));
-    if(answers.includes(test)) {
-      correct++;
-      console.log("%s\033[1;32m✓\033[0m\n", " ".repeat(width));
+    for(const entry of list) {
+      const [question, ...answers] = entry;
+      console.log("%s. %s", (++n).toString().padStart(3), question);
+
+      const test = await rl.question("   > ");
+      if(answers.includes(test)) {
+        console.log("   \033[1;32m✓\033[0m\n");
+      }
+      else {
+        console.log("   \033[1;31m✗\033[0m %s\n", answers[0]);
+        next.push(entry);
+      }
     }
-    else {
-      console.log("%s\033[1;31m✗\033[0m %s\n", " ".repeat(width), answers[0]);
-    }
+
+    list = shuffle(next);
   }
 
-  console.log("%d%%", Math.floor(correct * 100 / n));
+  if(n === QUESTIONS) {
+    console.log("\033[1;32mPerfect!\033[0m");
+  }
+  else {
+    console.log("\033[1;31mYou made mistakes. Keep trying!\033[0m");
+  }
 
   rl.close();
 }
